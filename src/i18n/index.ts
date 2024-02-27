a54f52384197592d2a43bc38bfd426f07ae0dbeb
MakoskiVictor <makoski.ed@gmail.com>
2024-02-27 11:54:54 -0300

    add fn getI18N

---
+++ src/i18n/index.ts
@@ -0,0 +1,17 @@
+import english from "./en.json";
+import spanish from "./es.json";
+
+const LANGUAGES = {
+  ENGLISH: "en",
+  SPANISH: "es",
+};
+
+export const getI18N = ({
+  currentLocale = "es",
+}: {
+  currentLocale?: string;
+}) => {
+  if (currentLocale === LANGUAGES.SPANISH) return spanish;
+  if (currentLocale === LANGUAGES.ENGLISH) return english;
+  return spanish;
+};
